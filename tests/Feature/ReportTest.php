c160c0471880de22e7d7652c9c4b4190a79077aa
devWillS <>
2022-04-11 22:13:01 +0900

    11-2 APIエンドポイントの作成

---
+++ tests/Feature/ReportTest.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ReportTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function api_customersにGETメソッドアクセスできる()
+    {
+        $response = $this->get('api/customers');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_customersにPOSTメソッドアクセスできる()
+    {
+        $response = $this->post('api/customers');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_customers_customer_idにGETメソッドアクセスできる()
+    {
+        $response = $this->get('api/customers/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_customers_customer_idにPUTメソッドアクセスできる()
+    {
+        $response = $this->put('api/customers/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_customers_customer_idにDELETEメソッドアクセスできる()
+    {
+        $response = $this->delete('api/customers/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_reportsにGETメソッドアクセスできる()
+    {
+        $response = $this->get('api/reports');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_reportsにPOSTメソッドアクセスできる()
+    {
+        $response = $this->post('api/reports');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_reports_report_idにGETメソッドアクセスできる()
+    {
+        $response = $this->get('api/reports/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_reports_report_idにPUTメソッドアクセスできる()
+    {
+        $response = $this->put('api/reports/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_reports_report_idにDELETEメソッドアクセスできる()
+    {
+        $response = $this->delete('api/reports/1');
+        $response->assertStatus(200);
+    }
+}
