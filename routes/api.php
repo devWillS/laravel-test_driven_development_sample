7abbed32107e49c30201e5f7e7d9be0a0285d46e
devWillS <>
2022-04-11 22:40:35 +0900

    11-4 データベースのテスト

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('customers', function () {
+    return response()->json(\App\Models\Customer::query()->select(['id', 'name'])->get());
 });
-Route::post('customers', function () {
+Route::post('customers', function (\Illuminate\Http\Request $request) {
+    if (!$request->json('name')) {
+        return response()->make('', \Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    $customer = new \App\Models\Customer();
+    $customer->name = $request->json('name');
+    $customer->save();
 });
 Route::get('customers/{customer_id}', function () {
 });
--- tests/Feature/ReportTest.php
+++ tests/Feature/ReportTest.php
@@ -4,10 +4,19 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use phpDocumentor\Reflection\Types\This;
 use Tests\TestCase;
 
 class ReportTest extends TestCase
 {
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('db:seed', ['--class' => 'TestDataSeeder']);
+    }
+
     /**
      * @test
      */
@@ -22,7 +31,10 @@ class ReportTest extends TestCase
      */
     public function api_customersにPOSTメソッドアクセスできる()
     {
-        $response = $this->post('api/customers');
+        $customer = [
+            'name' => 'customer_name',
+        ];
+        $response = $this->postJson('api/customers', $customer);
         $response->assertStatus(200);
     }
 
@@ -96,5 +108,57 @@ class ReportTest extends TestCase
     {
         $response = $this->delete('api/reports/1');
         $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_customersにGETメソッドでアクセスするとJSONが返却される()
+    {
+        $response = $this->get('api/customers');
+        $this->assertThat($response->content(), $this->isJson());
+    }
+
+    /**
+     * @test
+     */
+    public function api_customersにGETメソッドで取得できる顧客情報のJSON形式は要件通りである()
+    {
+        $response = $this->get('api/customers');
+        $customers = $response->json();
+        $customer = $customers[0];
+        $this->assertSame(['id', 'name'], array_keys($customer));
+    }
+
+    /**
+     * @test
+     */
+    public function api_customersに顧客名をPOSTするとcustomersテーブルにそのデータが追加される()
+    {
+        $params = [
+            'name' => '顧客名',
+        ];
+        $this->postJson('api/customers', $params);
+        $this->assertDatabaseHas('customers', $params);
+    }
+
+    /**
+     * @test
+     */
+    public function api_customersにnameが含まれない場合422UnprocessableEntityが返却される()
+    {
+        $params = [];
+        $response = $this->postJson('api/customers', $params);
+        $response->assertStatus(\Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /**
+     * @test
+     */
+    public function api_customersにnameが空の場合422UnprocessableEntityが返却される()
+    {
+        $params = ['name' => ''];
+        $response = $this->postJson('api/customers', $params);
+        $response->assertStatus(\Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY);
     }
 }
